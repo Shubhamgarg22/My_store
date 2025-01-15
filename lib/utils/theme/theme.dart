505e3b474914b2077d8e3fee1cbca78be98c034b
Shubham Garg <lkbshubham04@gmail.com>
2025-01-16 00:45:30 +0530

    commit

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:my_store/utils/theme/custom_themes/appbar_theme.dart';
+import 'package:my_store/utils/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:my_store/utils/theme/custom_themes/checkbox_theme.dart';
+import 'package:my_store/utils/theme/custom_themes/chip_theme.dart';
 import 'package:my_store/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:my_store/utils/theme/custom_themes/outlined_button_theme.dart';
+import 'package:my_store/utils/theme/custom_themes/text_field_theme.dart';
 import 'package:my_store/utils/theme/custom_themes/text_theme.dart';
 
 class MyAppTheme{
@@ -9,17 +15,29 @@ class MyAppTheme{
     useMaterial3: true,
     brightness: Brightness.light,
     primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
     textTheme: MyTextTheme.lightTextTheme,
+    chipTheme: MyChipTheme.lightChipTheme,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: MyAppBarTheme.lightAppBarTheme,
+    checkboxTheme: MyCheckBoxTheme.lightCheckBoxTheme,
+    bottomSheetTheme: MyBottomSheetTheme.lightBottomSheetTheme,
     elevatedButtonTheme:MyElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: MyOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: MyTextFormFieldTheme.lightInputDecorationTheme,
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     brightness: Brightness.dark,
     primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.black,
     textTheme: MyTextTheme.darkTextTheme,
-    elevatedButtonTheme:MyElevatedButtonTheme.blackElevatedButtonTheme,
+    chipTheme: MyChipTheme.darkChipTheme,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: MyAppBarTheme.darkAppBarTheme,
+    checkboxTheme: MyCheckBoxTheme.darkCheckBoxTheme,
+    bottomSheetTheme: MyBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme:MyElevatedButtonTheme.darkElevatedButtonTheme,
+    outlinedButtonTheme: MyOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: MyTextFormFieldTheme.darkInputDecorationTheme,
   );
 
 }
